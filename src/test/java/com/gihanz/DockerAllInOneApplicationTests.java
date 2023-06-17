2b5ff08d515f7c5361b081382353956b60121fe8
pamudi.h@informaticsint.com <pamudi.h@informaticsint.com>
2023-06-17 08:52:29 +0530

    first commit

---
+++ src/test/java/com/gihanz/DockerAllInOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gihanz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerAllInOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
